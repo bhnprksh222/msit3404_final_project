aaa15b77e7546761da81721bac38e19a84f348c3
Bhanu Prakash <bhnprksh222@gmail.com>
2025-12-01 21:03:03 -0500

    added the backend and the required docker files

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,7 @@
 console.log("DevOps Final Project Loaded Successfully!");
+
+document.addEventListener("DOMContentLoaded", () => {
+    const img = document.getElementById("backend-image");
+    img.src = "http://localhost:5000/image";
+});
+

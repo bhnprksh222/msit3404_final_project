5b5ad48db82f3a81dd2f2ac02862637a9cd0050a
Bhanu Prakash <bhnprksh222@gmail.com>
2025-12-02 17:14:46 -0500

    fixed port forward issue

--- frontend/script.js
+++ frontend/script.js
@@ -1,23 +1,12 @@
-// Fetch image from backend
-window.onload = async () => {
-    const img = document.getElementById("backend-image");
-    const loader = document.getElementById("loader");
-  
-    try {
-      const response = await fetch("http://127.0.0.1:8080/backend/static/img.jpg");
-      const blob = await response.blob();
-      img.src = URL.createObjectURL(blob);
-  
-      loader.style.display = "none";
-      img.style.display = "block";
-    } catch (err) {
-      loader.style.display = "none";
-      img.alt = "Failed to load image";
-    }
-  };
+const toggleBtn = document.getElementById("theme-toggle");
 
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
 
-
-console.log("DevOps Final Project Loaded Successfully!");
-
+  if (document.body.classList.contains("dark")) {
+    toggleBtn.textContent = "Light Mode";
+  } else {
+    toggleBtn.textContent = "Dark Mode";
+  }
+});
 
